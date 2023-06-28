3a1adc606013961813e845fc83c14bccc9a21651
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2023-06-28 20:48:24 +0300

    cleanDist, building

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
-const {src, dest, watch, parallel} = require('gulp')
+const {src, dest, watch, parallel, series} = require('gulp')
 
 const scss = require('gulp-sass')(require('sass'))
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean')
 
 function scripts() {
   return src('app/js/main.js')
+    .pipe(autoprefixer({overrideBrowserList: ['Last 10 version']}))
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(dest('app/js'))
@@ -35,9 +38,24 @@ function browsersync() {
 });
 }
 
+function cleanDist() {
+  return src('dist')
+  .pipe(clean())
+}
+
+function building() {
+  return src([
+    'app/css/styles.min.css',
+    'app/js/main.min.js',
+    'app/index.html'
+  ], {base: 'app'})
+  .pipe(dest('dist'))
+}
+
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
+exports.build = series(cleanDist, building);
 
 exports.default = parallel(styles, scripts, browsersync, watching)
