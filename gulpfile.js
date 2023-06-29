2648b184b5f8a4fe6971597e420d047a54aaee39
Evgeniy Strunevskiy <esstrunevskiy@gmail.com>
2023-06-29 10:08:34 +0300

    spriteSvg

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,40 @@ const uglify = require('gulp-uglify-es').default
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean')
+const avif = require('gulp-avif')
+const webp = require('gulp-webp')
+const imagemin = require('gulp-imagemin')
+const newer = require('gulp-newer')
+const svgSprite = require('gulp-svg-sprite')
+
+function images() {
+  return src(['app/images/src/*.*', '!app/images/src/*.svg'])
+  .pipe(newer('app/images/dist'))
+  .pipe(avif({quality: 50}))
+  
+  .pipe(src('app/images/src/*.*'))
+  .pipe(newer('app/images/dist'))
+  .pipe(webp())
+  
+  .pipe(src('app/images/src/*.*'))
+  .pipe(newer('app/images/dist'))
+  .pipe(imagemin())
+
+  .pipe(dest('app/images/dist'))
+}
+
+function sprite() {
+  return src('app/images/dist/*.svg')
+  .pipe(svgSprite({
+    mode: {
+      stack: {
+        sprite: '../sprite.svg',
+        example: true
+      }
+    }
+  }))
+  .pipe(dest('app/images/dist'))
+}
 
 function scripts() {
   return src('app/js/main.js')
@@ -25,18 +59,17 @@ function styles() {
 }
 
 function watching() {
-  watch(['app/scss/styles.scss'], styles)
-  watch(['app/js/main.js'], styles)
-  watch(['app/*.html']).on('change', browserSync.reload)
-}
-
-function browsersync() {
   browserSync.init({
     server: {
         baseDir: "app/"
     }
 });
+  watch(['app/scss/styles.scss'], styles)
+  watch(['app/images/src'], images)
+  watch(['app/js/main.js'], styles)
+  watch(['app/*.html']).on('change', browserSync.reload)
 }
+
 
 function cleanDist() {
   return src('dist')
@@ -46,6 +79,7 @@ function cleanDist() {
 function building() {
   return src([
     'app/css/styles.min.css',
+    'app/images/dist/*.*',
     'app/js/main.min.js',
     'app/index.html'
   ], {base: 'app'})
@@ -55,7 +89,8 @@ function building() {
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watching = watching;
-exports.browsersync = browsersync;
+exports.images = images;
+exports.sprite = sprite;
 exports.build = series(cleanDist, building);
 
-exports.default = parallel(styles, scripts, browsersync, watching)
+exports.default = parallel(styles, scripts, watching)
